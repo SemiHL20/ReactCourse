563070700e7952142e9b41c9ac2d6927e8f26ece
SemiHL20 <semilopezsanchez@gmail.com>
2025-07-03 13:15:55 +0200

    finished

--- arrays/src/App.js
+++ arrays/src/App.js
@@ -2,21 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const miArray = ()=> {
+    const numArray = [
+      {id: 1, value: 11},
+      {id: 2, value: 23},
+      {id: 3, value: 34},
+      {id: 4, value: 41},
+      {id: 5, value: 59}
+    ];
+
+    return(
+      <p>{numArray.map((item)=>{
+        return <span key={item.id}>{item.id} {"->"} {item.value} {"|"} </span>
+      })
+
+      }</p>
+    )
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Arrays in React</h1>
+        {miArray()}
       </header>
     </div>
   );

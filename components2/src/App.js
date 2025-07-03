bda25145a56cf19d6d7767622882e6ae4a713f0e
SemiHL20 <semilopezsanchez@gmail.com>
2025-07-03 16:33:57 +0200

    finish

--- components2/src/App.js
+++ components2/src/App.js
@@ -1,26 +1,47 @@
 import './App.css';
 import PropTypes from 'prop-types';
+import image from './assets/elephants.jpg';
 
 function App() {
 
   const usuario = {
     nombre: "Jose M.",
+    apellido: "López",
     edad: 43,
     ciudad: "Barcelona"
   };
 
   return (
-    <div className="App">
+    <div 
+      className="App" 
+      style={{ 
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center center',
+        backgroundRepeat: 'no-repeat'
+      }}>
       <Greeting user={usuario}/>
     </div>
   );
 }
 
 function Greeting(props) {
+
+  const { nombre, apellido, edad, ciudad } = props.user;
+
   return (
     <div>
-        <h1>This App is created by {props.user.nombre}</h1>
-        <p>who is {props.user.edad} and lives in {props.user.ciudad}</p>
+        {/*props.user.nombre && (<h1>This App is created by {props.user.nombre} {props.user.apellido}</h1>) 
+        //con el doble && si no se cumple la condición no muestra nada*/
+        nombre && apellido ? (<div><h1>This App is created by {nombre} {apellido}</h1>
+          <p>who is {edad} and lives in {ciudad}</p></div>) : 
+        (<h1>This App is created by an anonymous user</h1>)
+        }
     </div>
   );
 }
